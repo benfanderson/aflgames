99f58b7d096e86fceb2be916e093b0b74d975d67
Ben Anderson <ben.anderson@wanews.com.au>
2020-06-13 15:17:49 +0800

    Finished routes/ live game indicater

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes/routes');
 
-const controllers = require('./controllers/controller')
+// const controllers = require('./controllers/controller')
 
 const app = express();
 const port = process.env.PORT || '8000'
@@ -10,14 +11,16 @@ const port = process.env.PORT || '8000'
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/afl', routes);
 
-app.get('/', controllers.roundLinks)
-app.get('/round1', controllers.getRound1)
-app.get('/round2', controllers.getRound2)
-app.get('/round3', controllers.getRound3)
-app.get('/round4', controllers.getRound4)
-app.get('/round5', controllers.getRound5)
+// app.get('/', controllers.roundLinks)
+// app.get('/round1', controllers.getRound1)
+// app.get('/round2', controllers.getRound2)
+// app.get('/round3', controllers.getRound3)
+// app.get('/round4', controllers.getRound4)
+// app.get('/round5', controllers.getRound5)
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
+const router = express.Router();
+
 const app = express();
 
-const controllers = require('./controllers/controller')
+const controllers = require('../controllers/controller')
 
-app.get('/', controllers.roundLinks)
-app.get('/round1', controllers.getRound1)
-app.get('/round2', controllers.getRound2)
-app.get('/round3', controllers.getRound3)
-app.get('/round4', controllers.getRound4)
-app.get('/round5', controllers.getRound5)
+router.get('/', controllers.roundLinks)
+router.get('/round1', controllers.getRound1)
+router.get('/round2', controllers.getRound2)
+router.get('/round3', controllers.getRound3)
+router.get('/round4', controllers.getRound4)
+router.get('/round5', controllers.getRound5)
+
+module.exports = router;
