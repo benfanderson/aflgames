49ab09c6e590c91f05d4dc821ee272b2e91d0202
Ben Anderson <ben.anderson@wanews.com.au>
2020-06-18 20:31:07 +0800

    round controllers now created with single function

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,52 +14,38 @@ exports.roundLinks = function(req, res) {
     }   
 })();
 
-exports.getRound1 = function(req, res) {
-    const round =[];
+function getRoundInfo(roundNumber) {
+    const roundArray = [];
     gamesArray.forEach(element => {
-        if (element.round === 1 && element.year === 2020) {
-            round.push(element)
+        if (element.round === roundNumber && element.year === 2020) {
+            roundArray.push(element)
         }     
     });
+    return roundArray
+}
+
+
+exports.getRound1 = function(req, res) {
+    const round = getRoundInfo(1); 
     res.render('round_detail', {title: 'Round 1', round})
 }
 
 exports.getRound2 = function(req, res) {
-    const round =[];
-    gamesArray.forEach(element => {
-        if (element.round === 2 && element.year === 2020) {
-            round.push(element)
-        }     
-    });
+    const round = getRoundInfo(2);
     res.render('round_detail', {title: 'Round 2', round})
 }
 
 exports.getRound3 = function(req, res) {
-    const round =[];
-    gamesArray.forEach(element => {
-        if (element.round === 3 && element.year === 2020) {
-            round.push(element)
-        }     
-    });
+    const round = getRoundInfo(3);
     res.render('round_detail', {title: 'Round 3', round})
 }
 
 exports.getRound4 = function(req, res) {
-    const round =[];
-    gamesArray.forEach(element => {
-        if (element.round === 4 && element.year === 2020) {
-            round.push(element)
-        }     
-    });
+    const round = getRoundInfo(4);
     res.render('round_detail', {title: 'Round 4', round})
 }
 
 exports.getRound5 = function(req, res) {
-    const round =[];
-    gamesArray.forEach(element => {
-        if (element.round === 5 && element.year === 2020) {
-            round.push(element)
-        }     
-    });
+    const round = getRoundInfo(5);
     res.render('round_detail', {title: 'Round 5', round})
 }
