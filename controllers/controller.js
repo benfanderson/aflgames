565afb3714ea265f1d5e402639307cbb0d0cb719
Ben Anderson <ben.anderson@wanews.com.au>
2020-07-25 15:08:41 +0800

    Added basic club logo style

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,8 +5,6 @@ const { io } = require('../index');
 let interval;
 let gamesArray;
 
-
-
 (async() => {
   try {
     const response = await got('https://api.squiggle.com.au/?q=games');
@@ -20,24 +18,6 @@ let gamesArray;
 const getRoundInfo = function (round) {
   const roundArray = [];
   gamesArray.forEach( (element)=> {
-    if (element.round === round && element.year === 2020) {
-      element.date = tzMoment.tz(element.date, 'Australia/Melbourne');
-      element.date = moment(element.date).local();
-      element.time = moment(element.date).format('h:mma');
-      element.day = moment(element.date).format('ddd MMM D');
-      roundArray.push(element);
-    }
-  });
-  return roundArray
-};
-
-const emitLiveScores = async (socket, round) => {
-  try {
-    const response = await got('https://api.squiggle.com.au/?q=games');
-    const parsed = JSON.parse(response.body);
-    const gamesArray = parsed.games;
-    const roundArray = [];
-  gamesArray.forEach((element) => {
     if (element.round === round && element.year === 2020) {
       if (element.hteam === 'Adelaide') {
         element.hlogo = '/images/AdelaideCrows.svg'
@@ -114,6 +94,100 @@ const emitLiveScores = async (socket, round) => {
       } else if (element.ateam === 'Western Bulldogs') {
       element.alogo = '/images/WesternBulldogs.svg'
       }
+
+      element.date = tzMoment.tz(element.date, 'Australia/Melbourne');
+      element.date = moment(element.date).local();
+      element.time = moment(element.date).format('h:mma');
+      element.day = moment(element.date).format('ddd MMM D');
+      roundArray.push(element);
+    }
+  });
+  return roundArray
+};
+
+const emitLiveScores = async (socket, round) => {
+  try {
+    const response = await got('https://api.squiggle.com.au/?q=games');
+    const parsed = JSON.parse(response.body);
+    const gamesArray = parsed.games;
+    const roundArray = [];
+  gamesArray.forEach((element) => {
+    if (element.round === round && element.year === 2020) {
+      // if (element.hteam === 'Adelaide') {
+      //   element.hlogo = '/images/AdelaideCrows.svg'
+      // } else if (element.hteam === 'Brisbane Lions') {
+      //   element.hlogo = '/images/BrisbaneLions.svg'
+      // } else if (element.hteam === 'Carlton') {
+      //   element.hlogo = '/images/Carlton.svg'
+      // } else if (element.hteam === 'Collingwood') {
+      //   element.hlogo = '/images/Collingwood.svg'
+      // } else if (element.hteam === 'Essendon') {
+      //   element.hlogo = '/images/Essendon.svg'
+      // } else if (element.hteam === 'Fremantle') {
+      //   element.hlogo = '/images/Fremantle.svg'
+      // } else if (element.hteam === 'Geelong') {
+      //   element.hlogo = '/images/GeelongCats.svg'
+      // } else if (element.hteam === 'Gold Coast') {
+      //   element.hlogo = '/images/GoldCoastSuns.svg'
+      // } else if (element.hteam === 'Greater Western Sydney') {
+      //   element.hlogo = '/images/GWSGiants.svg'
+      // } else if (element.hteam === 'Hawthorn') {
+      //   element.hlogo = '/images/Hawthorn.svg'
+      // } else if (element.hteam === 'Melbourne') {
+      //   element.hlogo = '/images/Melbourne.svg'
+      // } else if (element.hteam === 'North Melbourne') {
+      //   element.hlogo = '/images/NorthMelbourne.svg'
+      // } else if (element.hteam === 'Port Adelaide') {
+      //   element.hlogo = '/images/PortAdelaide.svg'
+      // } else if (element.hteam === 'Richmond') {
+      //   element.hlogo = '/images/Richmond.svg'
+      // } else if (element.hteam === 'St Kilda') {
+      //   element.hlogo = '/images/StKilda.svg'
+      // } else if (element.hteam === 'Sydney') {
+      //   element.hlogo = '/images/SydneySwans.svg'
+      // } else if (element.hteam === 'West Coast') {
+      //   element.hlogo = '/images/WestCoastEagles.svg'
+      // } else if (element.hteam === 'Western Bulldogs') {
+      //   element.hlogo = '/images/WesternBulldogs.svg'
+      // }
+      
+      // if (element.ateam === 'Adelaide') {
+      // element.alogo = '/images/AdelaideCrows.svg'
+      // } else if (element.ateam === 'Brisbane Lions') {
+      // element.alogo = '/images/BrisbaneLions.svg'
+      // } else if (element.ateam === 'Carlton') {
+      // element.alogo = '/images/Carlton.svg'
+      // } else if (element.ateam === 'Collingwood') {
+      // element.alogo = '/images/Collingwood.svg'
+      // } else if (element.ateam === 'Essendon') {
+      // element.alogo = '/images/Essendon.svg'
+      // } else if (element.ateam === 'Fremantle') {
+      // element.alogo = '/images/Fremantle.svg'
+      // } else if (element.ateam === 'Geelong') {
+      // element.alogo = '/images/GeelongCats.svg'
+      // } else if (element.ateam === 'Gold Coast') {
+      // element.alogo = '/images/GoldCoastSuns.svg'
+      // } else if (element.ateam === 'Greater Western Sydney') {
+      // element.alogo = '/images/GWSGiants.svg'
+      // } else if (element.ateam === 'Hawthorn') {
+      // element.alogo = '/images/Hawthorn.svg'
+      // } else if (element.ateam === 'Melbourne') {
+      // element.alogo = '/images/Melbourne.svg'
+      // } else if (element.ateam === 'North Melbourne') {
+      // element.alogo = '/images/NorthMelbourne.svg'
+      // } else if (element.ateam === 'Port Adelaide') {
+      // element.alogo = '/images/PortAdelaide.svg'
+      // } else if (element.ateam === 'Richmond') {
+      // element.alogo = '/images/Richmond.svg'
+      // } else if (element.ateam === 'St Kilda') {
+      // element.alogo = '/images/StKilda.svg'
+      // } else if (element.ateam === 'Sydney') {
+      // element.alogo = '/images/SydneySwans.svg'
+      // } else if (element.ateam === 'West Coast') {
+      // element.alogo = '/images/WestCoastEagles.svg' 
+      // } else if (element.ateam === 'Western Bulldogs') {
+      // element.alogo = '/images/WesternBulldogs.svg'
+      // }
       
       element.date = tzMoment.tz(element.date, 'Australia/Melbourne');
       element.date = moment(element.date).local();
