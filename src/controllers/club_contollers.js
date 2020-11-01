ec0cdb9419df2c09cca66398b6635573f0093891
benfanderson <benfanderson81@gmail.com>
2020-11-01 14:19:15 +0800

    Controllers created via createController function

--- src/controllers/club_contollers.js
+++ src/controllers/club_contollers.js
@@ -2,245 +2,89 @@
 import "regenerator-runtime/runtime.js";
 import { createGamesArray, fetchLiveScores } from '../api_call';
 
+async function createController(gamesParam, gamesProp, res) {
+  try {
+    const gamesArray = await createGamesArray(gamesParam, gamesProp);
+    fetchLiveScores(gamesParam, gamesProp)
+    res.render('games_detail', { title: gamesParam, gamesArray });
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+
+}
+
 exports.clubLinks = function (req, res) {
   res.render('club_index', { title: 'Games by club'});
 };
 
 exports.adelaide = function (req, res) {
-  (async()=>{
-    try {
-      const gamesArray = await createGamesArray('Adelaide', ['hteam','ateam']);
-      fetchLiveScores('Adelaide', ['hteam', 'ateam'])
-      res.render('games_detail', { title: 'Adelaide', gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  })()
+  createController('Adelaide', ['hteam','ateam'], res)
 };
 
 exports.brisbane = function (req, res) {
-  (async() => {
-    try {
-      const gamesArray = await createGamesArray('Brisbane Lions', ['hteam', 'ateam']);
-      fetchLiveScores('Brisbane Lions', ['hteam', 'ateam'])
-    res.render('games_detail', { title: 'Brisbane', gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  }
-  )()
+  createController('Brisbane Lions', ['hteam','ateam'], res)
 };
 
 exports.carlton = function (req, res) {
-  (async() => {
-    try {
-      const gamesArray = await createGamesArray('Carlton', ['hteam', 'ateam']);
-      fetchLiveScores('Carlton', ['hteam', 'ateam'])
-    res.render('games_detail', { title: 'Carlton', gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  }
-  )()
+  createController('Carlton', ['hteam','ateam'], res)
 };
 
-  // exports.collingwood = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Collingwood', ['hteam', 'ateam']);
-  //       fetchLiveScores('Collingwood')
-  //     res.render('games_detail', { title: 'Collingwood', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.collingwood = function (req, res) {
+  createController('Collingwood', ['hteam','ateam'], res)
+};
 
-  // exports.essendon = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Essendon', ['hteam', 'ateam']);
-  //       fetchLiveScores('Essendon')
-  //     res.render('games_detail', { title: 'Essendon', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.essendon = function (req, res) {
+  createController('Essendon', ['hteam','ateam'], res)
+};
 
-  // exports.fremantle = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Fremantle', ['hteam', 'ateam']);
-  //       fetchLiveScores('Fremantle')
-  //     res.render('games_detail', { title: 'Fremantle', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.fremantle = function (req, res) {
+  createController('Fremantle', ['hteam','ateam'], res)
+};
 
-  // exports.geelong = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Geelong', ['hteam', 'ateam']);
-  //       fetchLiveScores('Geelong')
-  //     res.render('games_detail', { title: 'Geelong', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.geelong = function (req, res) {
+  createController('Geelong', ['hteam','ateam'], res)
+};
 
-  // exports.goldcoast = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Gold Coast', ['hteam', 'ateam']);
-  //       fetchLiveScores('Gold Coast')
-  //     res.render('games_detail', { title: 'Gold Coast', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.goldcoast = function (req, res) {
+  createController('Gold Coast', ['hteam','ateam'], res)
+};
 
-  // exports.gws = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Greater Western Sydney', ['hteam', 'ateam']);
-  //       fetchLiveScores('Greater Western Sydney')
-  //     res.render('games_detail', { title: 'Greater Western Sydney', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.gws = function (req, res) {
+  createController('Greater Western Sydney', ['hteam','ateam'], res)
+};
 
-  // exports.hawthorn = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Hawthorn', ['hteam', 'ateam']);
-  //       fetchLiveScores('Hawthorn')
-  //     res.render('games_detail', { title: 'Hawthorn', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.hawthorn = function (req, res) {
+  createController('Hawthorn', ['hteam','ateam'], res)
+};
 
-  // exports.melbourne = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('Melbourne', ['hteam', 'ateam']);
-  //       fetchLiveScores('Melbourne')
-  //     res.render('games_detail', { title: 'Melbourne', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
-  // exports.northmelbourne = function (req, res) {
-  //   (async() => {
-  //     try {
-  //       const gamesArray = await createGamesArray('North Melbourne', ['hteam', 'ateam']);
-  //       fetchLiveScores('North Melbourne')
-  //     res.render('games_detail', { title: 'North Melbourne', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   }
-  //   )()
-  // };
+exports.melbourne = function (req, res) {
+  createController('Melbourne', ['hteam','ateam'], res)
+};
 
-  // exports.portadelaide = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('Port Adelaide', ['hteam','ateam']);
-  //       fetchLiveScores('Port Adelaide')
-  //       res.render('games_detail', { title: 'Port Adelaide', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.northmelbourne = function (req, res) {
+  createController('North Melbourne', ['hteam','ateam'], res)
+};
 
-  // exports.richmond = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('Richmond', ['hteam','ateam']);
-  //       fetchLiveScores('Richmond')
-  //       res.render('games_detail', { title: 'Richmond', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.portadelaide = function (req, res) {
+  createController('Port Adelaide', ['hteam','ateam'], res)
+};
 
-  // exports.sydney = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('Sydney', ['hteam','ateam']);
-  //       fetchLiveScores('Sydney')
-  //       res.render('games_detail', { title: 'Sydney', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.richmond = function (req, res) {
+  createController('Richmond', ['hteam','ateam'], res)
+};
 
-  // exports.stkilda = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('St Kilda', ['hteam','ateam']);
-  //       fetchLiveScores('St Kilda')
-  //       res.render('games_detail', { title: 'St Kilda', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.sydney = function (req, res) {
+  createController('Sydney', ['hteam','ateam'], res)
+};
 
-  // exports.stkilda = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('St Kilda', ['hteam','ateam']);
-  //       fetchLiveScores('St Kilda')
-  //       res.render('games_detail', { title: 'St Kilda', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.stkilda = function (req, res) {
+  createController('St Kilda', ['hteam','ateam'], res)
+};
 
-  // exports.westcoast = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('West Coast', ['hteam','ateam']);
-  //       fetchLiveScores('West Coast')
-  //       res.render('games_detail', { title: 'West Coast', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
+exports.westcoast = function (req, res) {
+  createController('West Coast', ['hteam','ateam'], res)
+};
 
-  // exports.westernbulldogs = function (req, res) {
-  //   (async()=>{
-  //     try {
-  //       const gamesArray = await createGamesArray('Western Bulldogs', ['hteam','ateam']);
-  //       fetchLiveScores('Western Bulldogs')
-  //       res.render('games_detail', { title: 'Western Bulldogs', gamesArray });
-  //     } catch (error) {
-  //       console.error(`Error: ${error.code}`);
-  //     }
-  //   })()
-  // };
-  
+exports.westernbulldogs = function (req, res) {
+  createController('Western Bulldogs', ['hteam','ateam'], res)
+};
--- src/controllers/round_controller.js
+++ src/controllers/round_controller.js
@@ -1,6 +1,16 @@
 import "regenerator-runtime/runtime.js";
 import { createGamesArray, fetchLiveScores } from '../api_call';
 
+async function createController(gamesParam, gamesProp, res) {
+  try {
+    const gamesArray = await createGamesArray(gamesParam, gamesProp);
+    fetchLiveScores(gamesParam, gamesProp)
+    res.render('games_detail', { title: `Round ${gamesParam}`, gamesArray });
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+}
+
 exports.index = function (req, res) {
   res.render('index', { title: 'Season 2020' });
 };
@@ -10,203 +20,73 @@ exports.roundLinks = function (req, res) {
 };
 
 exports.getRound1 = function (req, res) {
-  (async() => {
-    try {
-      const gamesArray = await createGamesArray(1, ['round']);
-    fetchLiveScores(1, ['round'])
-    res.render('games_detail', { title: `Round ${1}`, gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  })()
+  createController(1, ['round'], res)
 };
 
 exports.getRound2 = function (req, res) {
-  (async() => {
-    try {
-      const gamesArray = await createGamesArray(2, ['round']);
-    fetchLiveScores(2, ['round'])
-    res.render('games_detail', { title: `Round ${2}`, gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  })()
+  createController(2, ['round'], res)
 };
 
 exports.getRound3 = function (req, res) {
-  (async() => {
-    try {
-      const gamesArray = await createGamesArray(3, ['round']);
-    fetchLiveScores(3, ['round'])
-    res.render('games_detail', { title: `Round ${3}`, gamesArray });
-    } catch (error) {
-      console.error(`Error: ${error.code}`);
-    }
-  })()
+  createController(3, ['round'], res)
 };
 
-// exports.getRound4 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(4, ['round']);
-//     fetchLiveScores(4)
-//     res.render('games_detail', { title: `Round ${4}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound5 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(5, ['round']);
-//     fetchLiveScores(5)
-//     res.render('games_detail', { title: `Round ${5}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound6 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(6, ['round']);
-//     fetchLiveScores(6)
-//     res.render('games_detail', { title: `Round ${6}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound7 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(7, ['round']);
-//     fetchLiveScores(7)
-//     res.render('games_detail', { title: `Round ${7}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound8 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(8, ['round']);
-//     fetchLiveScores(8)
-//     res.render('games_detail', { title: `Round ${8}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound9 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(9, ['round']);
-//     fetchLiveScores(9)
-//     res.render('games_detail', { title: `Round ${9}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound10 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(10, ['round']);
-//     fetchLiveScores(10)
-//     res.render('games_detail', { title: `Round ${10}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound11 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(11, ['round']);
-//     fetchLiveScores(11)
-//     res.render('games_detail', { title: `Round ${11}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound12 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(12, ['round']);
-//     fetchLiveScores(12)
-//     res.render('games_detail', { title: `Round ${12}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound13 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(13, ['round']);
-//     fetchLiveScores(13)
-//     res.render('games_detail', { title: `Round ${13}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound14 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(14, ['round']);
-//     fetchLiveScores(14)
-//     res.render('games_detail', { title: `Round ${14}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound15 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray= await createGamesArray(15, ['round']);
-//     fetchLiveScores(15)
-//     res.render('games_detail', { title: `Round ${15}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound16 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(16, ['round']);
-//     fetchLiveScores(16)
-//     res.render('games_detail', { title: `Round ${16}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound17 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(17, ['round']);
-//     fetchLiveScores(17)
-//     res.render('games_detail', { title: `Round ${17}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
-// exports.getRound18 = function (req, res) {
-//   (async() => {
-//     try {
-//       const gamesArray = await createGamesArray(18, ['round']);
-//     fetchLiveScores(18)
-//     res.render('games_detail', { title: `Round ${18}`, gamesArray });
-//     } catch (error) {
-//       console.error(`Error: ${error.code}`);
-//     }
-//   })()
-// };
+exports.getRound4 = function (req, res) {
+  createController(4, ['round'], res)
+};
+
+exports.getRound5 = function (req, res) {
+  createController(5, ['round'], res)
+};
+
+exports.getRound6 = function (req, res) {
+  createController(6, ['round'], res)
+};
+
+exports.getRound7 = function (req, res) {
+  createController(7, ['round'], res)
+};
+
+exports.getRound8 = function (req, res) {
+  createController(8, ['round'], res)
+};
+
+exports.getRound9 = function (req, res) {
+  createController(9, ['round'], res)
+};
+
+exports.getRound10 = function (req, res) {
+  createController(10, ['round'], res)
+};
+
+exports.getRound11 = function (req, res) {
+  createController(11, ['round'], res)
+};
+
+exports.getRound12 = function (req, res) {
+  createController(12, ['round'], res)
+};
+
+exports.getRound13 = function (req, res) {
+  createController(13, ['round'], res)
+};
+
+exports.getRound14 = function (req, res) {
+  createController(14, ['round'], res)
+};
+
+exports.getRound15 = function (req, res) {
+  createController(15, ['round'], res)
+};
+
+exports.getRound16 = function (req, res) {
+  createController(16, ['round'], res)
+};
+
+exports.getRound17 = function (req, res) {
+  createController(17, ['round'], res)
+};
+
+exports.getRound18 = function (req, res) {
+  createController(18, ['round'], res)
+};
